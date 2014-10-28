388d96250187e605f880dbb3d163f5ff0485ed44
gorbinphilip <gorbinphilip@gmail.com>
2014-10-28 17:36:53 +0530

    added field_id to fields

--- pyregistrar/registrar.py
+++ pyregistrar/registrar.py
@@ -9,7 +9,8 @@ class Field(object):
     TYPE_TEXT_MULTILINE="text:multiline"
     TYPE_TEXT_PASSWORD="text:password"
     
-    def __init__(self, field_name, field_type, description=None):
+    def __init__(self, field_id, field_name, field_type, description=None):
+        self.field_id=field_id
         self.field_name=field_name
         self.field_type=field_type
         self.field_description=description
--- pyregistrar/test/test_registrar.py
+++ pyregistrar/test/test_registrar.py
@@ -1,9 +1,10 @@
 from .. import registrar
 
 def test_fields():
-    field=registrar.Field("test_field", registrar.Field.TYPE_NONE, "this is a test field")
+    field=registrar.Field("test_field", "test field", registrar.Field.TYPE_NONE, "this is a test field")
     assert isinstance(field, registrar.Field)==True
-    assert field.field_name=="test_field"
+    assert field.field_id=="test_field"
+    assert field.field_name=="test field"
     assert field.field_type=="none"
     assert field.field_description=="this is a test field"
 
