8d5ceef5d37f7d5fbd24886fd31cd862b87ab2fc
gorbinphilip <gorbinphilip@gmail.com>
2014-10-28 16:20:36 +0530

    added Field to registrar

---
+++ pyregistrar/registrar.py
@@ -0,0 +1,16 @@
+
+
+
+class Field(object):
+    """Provides standard fields to represent attributes"""
+    TYPE_NONE="none"
+    TYPE_NUMBER="number"
+    TYPE_TEXT_ONELINE="text:oneline"
+    TYPE_TEXT_MULTILINE="text:multiline"
+    TYPE_TEXT_PASSWORD="text:password"
+    
+    def __init__(self, field_name, field_type, description=None):
+        self.field_name=field_name
+        self.field_type=field_type
+        self.field_description=description
+
