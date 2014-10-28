d34743050ac7128454875fb88b7ac584993814f3
gorbinphilip <gorbinphilip@gmail.com>
2014-10-28 18:00:54 +0530

    added Alien model to models

--- pyregistrar/models.py
+++ pyregistrar/models.py
@@ -7,11 +7,33 @@ class Model(object):
     def __init__(self):
         raise NotImplementedError()
 
-    def set(self, field, value):
+    def set(self, field_id, value):
         raise NotImplementedError()
 
-    def get(self, field):
+    def get(self, field_id):
         raise NotImplementedError()
 
     def get_list_fields(self):
         raise NotImplementedError()
+
+
+class Alien(Model):
+    """Model for the alien object which can hold alien details"""
+
+    def __init__(self):
+        """initializes alien with required fields"""
+        self.fields=[]
+        self.fields.append(Field("code_name", "Code Name", Field.TYPE_TEXT_ONELINE, "Name detail of the Alien"))
+        self.fields.append(Field("blood_color", "Blood Colour", Field.TYPE_TEXT_ONELINE, "Colour of the blood"))
+        self.fields.append(Field("no_antenna", "No.s  antenna", Field.TYPE_NUMBER, "Number of antenna"))
+        self.fields.append(Field("no_legs", "No.s  legs", Field.TYPE_NUMBER, "Number of legs"))
+        self.fields.append(Field("home_planet", "Home Planet", Field.TYPE_NUMBER, "Name of the home planet"))
+
+    def set(self, field_id, value):
+        self.field_id=value
+
+    def get(self, field_id):
+        return self.field_id
+
+    def get_list_fields(self):
+        return self.fields
--- pyregistrar/test/test_models.py
+++ pyregistrar/test/test_models.py
@@ -1,8 +1,17 @@
-from ..models import Model
+from ..models import Model, Alien
+from ..registrar import Field
 import pytest
 
-def test_model():
+def test_Model():
     with pytest.raises(NotImplementedError):
         tmp=Model()
 
-
+def test_Alien():
+    tmp=Alien()
+    assert isinstance(tmp, Alien)==True
+    assert type(tmp.get_list_fields())==list
+    for field in tmp.fields:
+        assert isinstance(field, Field)
+    for field in tmp.fields:
+        tmp.set(field.field_id, "test value")
+        assert tmp.get(field.field_id)=="test value"
