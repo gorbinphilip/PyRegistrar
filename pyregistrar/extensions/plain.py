1ac3eca4ba494be0130a486222beb7a1f2928e3a
gorbinphilip <gorbinphilip@gmail.com>
2014-10-29 10:03:40 +0530

    fixed plain extension

--- pyregistrar/extensions/plain.py
+++ pyregistrar/extensions/plain.py
@@ -3,7 +3,7 @@ class Export(object):
     def __init__(self):
         pass
 
-    def export(data_list, file_name):
+    def export(self, data_list, file_name):
         file=open(file_name, "w")
         for k, v in data_list:
             file.write(k+"\t:->\t"+v)
---
+++ pyregistrar/test/test_plain.py
@@ -0,0 +1,12 @@
+from  ..extensions import plain
+import os
+
+def test_plain():
+    p=plain.Export()
+    file="/tmp/test.txt"
+    data=[("name","me"), ("age","0")]
+    p.export(data, file)
+    f=open(file)
+    data=f.read()
+    assert data.__contains__("name")
+    os.remove(file)
