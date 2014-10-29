bda6174783aadd1e624624af57974d8be5948cb3
gorbinphilip <gorbinphilip@gmail.com>
2014-10-29 09:43:37 +0530

    added plain extension

---
+++ pyregistrar/extensions/plain.py
@@ -0,0 +1,10 @@
+class Export(object):
+
+    def __init__(self):
+        pass
+
+    def export(data_list, file_name):
+        file=open(file_name, "w")
+        for k, v in data_list:
+            file.write(k+"\t:->\t"+v)
+        file.close()
